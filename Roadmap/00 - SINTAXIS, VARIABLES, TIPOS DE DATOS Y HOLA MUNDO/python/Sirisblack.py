9756f9b17e3f0bdd0067521b7c0a4b58db37a127
Wysty <flsirisblack25@gmail.com>
2024-09-20 11:18:11 +0200

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Sirisblack.py
@@ -0,0 +1,26 @@
+# Sitio oficial de Python https://www.python.org
+
+# ¡¡¡ MUCHAS GRACIAS !!!
+# NOTA PARA LAS CORRECCIONES, ENTIENDO QUE ESTO COMENZO A PRINCIPIOS DEL 2024, Y NO HABRA FEEDBACKS.
+
+# Esto es un comentario en una linea.
+
+"""
+Esto es un comentario
+en varias
+líneas.
+Estas líneas no 
+se ejecutaran.
+Se utilizan tambien
+las comillas simples '''
+"""
+mi_variable = "Fernando"            # Esta variable es tipo string (str)
+CONSTANTE = "No es una constante."  # Por convención le estoy pidiendo por favor a los compañeros, no toques esta variable "TE REVIENTO"
+
+mi_entero = 49                      # Esto es un INT, es un número entero
+mi_decimal = 1.23                   # Esto es un FLOAT, es un número decimal
+mi_booleano = True                  # Esto es un BOOL, solo indica si es True (verdadero)
+mi_booleano = False                 # o False (Falso)
+mi_cadena_de_texto = "Estoy aprendiendo Python desde cero."     # Esto es una cadena de texto STR o string y se puede usar comilla simple ('Hola, Python')
+
+print("¡Hola, Python!")
